6b132ac5651d65a76c5f897d48618accf7fce952
leishifang <353646121@qq.com>
2016-03-21 14:49:25 +0800

    login

---
+++ app/src/main/java/com/example/giggle/oschina2/ui/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.giggle.oschina2.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.giggle.oschina2.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
